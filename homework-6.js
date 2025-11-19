e3df0135b7142a66170d2172c61e41df89ae5682
Abdurahman <kkurbanov0502@mail.ru>
2025-11-19 13:18:59 +0300

    corrected the formatting

--- homework-6.js
+++ homework-6.js
@@ -1,4 +1,4 @@
-// 3.Создать объект основе ваших данных 
+// 3.Создать объект основе ваших данных
 
 const profile = {
   name: "Adburrohman",
@@ -8,8 +8,8 @@ const profile = {
   city: "Derbent",
   job: "Сrane operator",
   hobby: "Fishing",
-  mail: "kkurbanov@gmail.com"
-}
+  mail: "kkurbanov@gmail.com",
+};
 console.log(profile);
 
 // .4 Создать объект который будет хранить данные об автомобиле и добавить дополниетльное свойство...
@@ -20,20 +20,24 @@ const car = {
   year: 2020,
   color: "Black",
   mileage: 15000,
-}
+};
 car.owner = profile;
 console.log(car);
 
 // 5. Написать функцию которая аргументом будет принимать объект описанный в пунке 4...
 
 function setMaxSpeed(carObj, topSpeed) {
-  if (!carObj.hasOwnProperty('topSpeed')) {
-  carObj.topSpeed = topSpeed;
-  console.log(`Максимальная скорость автомобиля ${carObj.brand} ${carObj.model} установлена на ${topSpeed} км/ч.`);
+  if (!carObj.hasOwnProperty("topSpeed")) {
+    carObj.topSpeed = topSpeed;
+    console.log(
+      `Максимальная скорость автомобиля ${carObj.brand} ${carObj.model} установлена на ${topSpeed} км/ч.`
+    );
   } else {
-  console.log(`Максимальная скорость автомобиля ${carObj.brand} ${carObj.model} уже установлена на ${carObj.topSpeed} км/ч.`);
+    console.log(
+      `Максимальная скорость автомобиля ${carObj.brand} ${carObj.model} уже установлена на ${carObj.topSpeed} км/ч.`
+    );
   }
-};
+}
 setMaxSpeed(car, 180);
 setMaxSpeed(car, 200);
 console.log(car);
@@ -47,7 +51,18 @@ printCarInfo(car, "brand");
 
 // 7. Массив продуктов
 
-const products = ["milk", "bread", "cheese", "butter", "tea", "sugar", "coffee", "candies", "sausage", "sour cream"];
+const products = [
+  "milk",
+  "bread",
+  "cheese",
+  "butter",
+  "tea",
+  "sugar",
+  "coffee",
+  "candies",
+  "sausage",
+  "sour cream",
+];
 
 // 8.Массив из книг
 
@@ -57,69 +72,68 @@ const library = [
     author: "Лев Толстой",
     year: 1867,
     coverColor: "Красный",
-    genre:  "Историческая Фантастика",
+    genre: "Историческая Фантастика",
   },
   {
     bookTitle: "Метель",
     author: "Александр Пушкин",
     year: 1830,
     coverColor: "Синий",
-    genre:  "Романтическая Литература",
+    genre: "Романтическая Литература",
   },
   {
     bookTitle: "Идиот",
     author: "Федор Достоевский",
     year: 1869,
     coverColor: "Зеленый",
-    genre:  "Роман",  
-  }
+    genre: "Роман",
+  },
 ];
+library.push({
+    bookTitle: "Пищи сокращай",
+    author: "Максим Ильяхов Людмила Сарычева",
+    year: 2022,
+    coverColor: "Оранжевый",
+    genre: "Саморазвитие",
+});
 
- library.push({
-  bookTitle: "Пищи сокращай",
-  author: "Максим Ильяхов Людмила Сарычева",
-  year: 2022,
-  coverColor: "Оранжевый",
-  genre: "Саморазвитие"
- })
- 
- console.log(library);
+console.log(library);
 
- // 9. Создать еще массив книг и объединить с другим массивом 
+// 9. Создать еще массив книг и объединить с другим массивом
 
- const fictionBooks = [
+const fictionBooks = [
   {
-  bookTitle: "Дюна",
-  author: "Фрэнк Герберт",
-  year: 1965,
-  coverColor: "Желтый",
-  genre: "Научная фантастика"
- },
+    bookTitle: "Дюна",
+    author: "Фрэнк Герберт",
+    year: 1965,
+    coverColor: "Желтый",
+    genre: "Научная фантастика",
+  },
 
- {
-  bookTitle: "Нейромант",
-  author: "Уильям Гибсон",
-  year: 1984,
-  coverColor: "Серый",
-  genre: "Киберпанк"
- },
- {
-  bookTitle: "Марсианин",
-  author: "Энди Вейер",
-  year: 2011,
-  coverColor: "Красный",
-  genre: "Научная фантастика"
- }
-]
+  {
+    bookTitle: "Нейромант",
+    author: "Уильям Гибсон",
+    year: 1984,
+    coverColor: "Серый",
+    genre: "Киберпанк",
+  },
+  {
+    bookTitle: "Марсианин",
+    author: "Энди Вейер",
+    year: 2011,
+    coverColor: "Красный",
+    genre: "Научная фантастика",
+  },
+];
 const books = [...library, ...fictionBooks];
 console.log(books);
 
-// 10. Создать функцию которая принимает массив сущностей 
+// 10. Создать функцию которая принимает массив сущностей
 
 function addProperty(booksarray) {
-  return booksarray.map(book => ({ 
-  ...book,
-  isRare: book.year < 2000
+    return booksarray.map((book) => ({
+    ...book,
+    isRare: book.year < 2000,
   }));
 }
 const newBooks = addProperty(books);
