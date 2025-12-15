35e8093a7e211dcd66c1e50180a32cc79d9e0144
Abdurahman <kkurbanov0502@mail.ru>
2025-12-15 23:20:10 +0300

    Task with footer and modal

---
+++ homework-9.js
@@ -0,0 +1,62 @@
+// 4 К форме в footer добавить логику и валидацию
+
+const footerForm = document.querySelector('#footerForm')
+footerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+  const mailForm = event.target
+  const formData = new FormData(mailForm)
+  const data = Object.fromEntries(formData.entries());
+  console.log(data)
+})
+
+// 5 создать форму для регистрации 
+
+const openModal = document.getElementById('openModal');
+const  closeModalBtn = document.getElementById('closeModalBtn');
+const overlayModal = document.getElementById('overlayModal');
+
+openModal.addEventListener('click', () => {
+  overlayModal.classList.add('modal-showed'); 
+})
+
+closeModalBtn.addEventListener('click',() =>{
+  overlayModal.classList.remove('modal-showed')
+})
+
+overlayModal.addEventListener('click',() => {
+  if (event.target === overlayModal) {
+    overlayModal.classList.remove('modal-showed');
+  }
+});
+
+// 6 Создать форму для регистрации внутри модального окна.
+
+const registerForm = document.getElementById('registerForm');
+let user = null;
+
+registerForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+if (!registerForm.checkValidity()) {
+alert('Форма заполнена неверно');
+  return;
+}
+
+const formData = new FormData(registerForm);
+const data = Object.fromEntries(formData.entries());
+if (data.password !== data.repeatPassword) {
+alert('Пароли не совпадают');
+  return;
+}
+
+user = {
+firstName: data.firstName,
+lastName: data.lastName,
+birthDate: data.birthDate,
+login: data.login,
+createdOn: new Date(),
+};
+
+overlayModal.classList.remove('modal-showed');
+registerForm.reset();
+});
