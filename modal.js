3e50dec66f0d5eb5cb6c46ffed79ca6d1467f208
Abdurahman <kkurbanov0502@mail.ru>
2026-01-10 16:58:56 +0300

    bug fixes

--- modal.js
+++ modal.js
@@ -6,19 +6,18 @@ this.closeBtn = this.overlay.querySelector(".modal-off");
 this.closeBtn.addEventListener("click", () => this.close());
 this.overlay.addEventListener("click", (event) => {
 if (event.target === this.overlay) {
-  this.close();
- }
- });
- }
+this.close();
+}});
+}
 open() {
-  this.overlay.classList.add("overlay--visible");
-  this.modal.classList.add("modal--visible");
+this.overlay.classList.add("overlay--visible");
+this.modal.classList.add("modal--visible");
 }
 close() {
-  this.overlay.classList.remove("overlay--visible");
-  this.modal.classList.remove("modal--visible");
+this.overlay.classList.remove("overlay--visible");
+this.modal.classList.remove("modal--visible");
 }
-  isOpen() {
-  return this.modal.classList.contains("modal--visible");
-  }
+isOpen() {
+return this.modal.classList.contains("modal--visible");
+}
 }
