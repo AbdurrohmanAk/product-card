13e07b507aad0bc7971f51a7427a24b97d7b209b
Abdurahman <kkurbanov0502@mail.ru>
2026-01-15 18:23:37 +0300

    bug fixes

--- modal.js
+++ modal.js
@@ -1,23 +1,24 @@
 export class Modal {
-constructor(overlayId) {
-this.overlay = document.getElementById(overlayId);
-this.modal = this.overlay.querySelector(".modal-home");
-this.closeBtn = this.overlay.querySelector(".modal-off");
-this.closeBtn.addEventListener("click", () => this.close());
-this.overlay.addEventListener("click", (event) => {
-if (event.target === this.overlay) {
-this.close();
-}});
-}
-open() {
-this.overlay.classList.add("overlay--visible");
-this.modal.classList.add("modal--visible");
-}
-close() {
-this.overlay.classList.remove("overlay--visible");
-this.modal.classList.remove("modal--visible");
-}
-isOpen() {
-return this.modal.classList.contains("modal--visible");
-}
+  constructor(overlayId) {
+    this.overlay = document.getElementById(overlayId);
+    this.modal = this.overlay.querySelector('.modal-home');
+    this.closeBtn = this.overlay.querySelector('.modal-off');
+    this.closeBtn.addEventListener('click', () => this.close());
+    this.overlay.addEventListener('click', (event) => {
+      if (event.target === this.overlay) {
+        this.close();
+      }
+    });
+  }
+  open() {
+  this.overlay.classList.add('overlay--visible');
+  this.modal.classList.add('modal--visible');
+  }
+  close() {
+  this.overlay.classList.remove('overlay--visible');
+  this.modal.classList.remove('modal--visible');
+  }
+  isOpen() {
+    return this.modal.classList.contains('modal--visible');
+  }
 }
